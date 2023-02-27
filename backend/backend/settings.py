d721342fa34708ef40827b93654c998f30236a51
Sparcos08 <mohamedaziz.chibani0@gmail.com>
2023-02-27 17:58:56 +0000

    kljqskljqs

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -134,9 +134,9 @@ DATABASES = {
 #Emailing settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_FROM = 'zicklerchristopher@gmail.com'
-EMAIL_HOST_USER = 'zicklerchristopher@gmail.com'
-EMAIL_HOST_PASSWORD = 'lmfmihodqahihyeu'
+EMAIL_FROM = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True 
 PASSWORD_RESET_TIMEOUT = 14400
